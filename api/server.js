44d68c9e5052f851634975b9e48c247187444836
sunrise8vn <sunrise8vn@gmail.com>
2023-12-15 16:39:15 +0700

    Init

--- api/server.js
+++ api/server.js
@@ -18,17 +18,19 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 // Add this before server.use(router)
-// server.use(
-//   jsonServer.rewriter({
-//     '/api/*': '/$1',
-//   })
-// );
+server.use(
+  jsonServer.rewriter({
+    // '/api/*': '/$1',
+  })
+);
 
 server.use(router);
 
-// server.listen(3000, () => {
-//   console.log('JSON Server is running');
-// });
+const PORT = 3300;
+
+server.listen(PORT, () => {
+  console.log('JSON Server is running');
+});
 
 // Export the Server API
 module.exports = server;
